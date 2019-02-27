85d7fdbe1a0ba3463123af6a5c1995db4e04d02c
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2019-02-27 10:03:59 -0600

    update

--- Media 1.2.1/src/Song.java
+++ Media 1.2.1/src/Song.java
@@ -1,60 +1,75 @@
 
 /**
- * Write a description of class Song here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * A song to save to the library
+ * 
+ * @author ckinnard
+ * @version 3/5/16
  */
 public class Song
 {
-    // instance variables - replace the example below with your own
-    private int rating;
-    private String title;
-    private double price = 0.0;
-    private boolean favorite;
-    private double totalCost;
     private static int numSongsInLibrary;
-    public Song() {
-        rating = 0;
-        title = "";
-    }
+
+    private String title;
+    private int rating;
+    private boolean favorite;
+    private double  price;
     /**
      * Constructor for objects of class Song
      */
-    public Song(String title, double price)
+    public Song()
     {
         // initialise instance variables
-        this.title = title;
-        this.price = price;
-        this.rating = rating;
+        rating = 0;
+        title = "";
+        price = 0.0;
+
         numSongsInLibrary = numSongsInLibrary + 1;
     }
+
+    public Song(String title, double price) {
+        this.title = title;
+        this.price = price;
+
+        numSongsInLibrary = numSongsInLibrary + 1;
+    }
+
     public Song(String title, double price, int rating) {
         this.title = title;
         this.price = price;
         this.rating = rating;
-        
+
         numSongsInLibrary = numSongsInLibrary + 1;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String t) {
         title = t;
     }
-    public Integer getRating(){
+
+    public int getRating() {
         return rating;
     }
-    public void setRating(int r){
+
+    public void setRating(int r) {
         rating = r;
     }
-    public Double getPrice(){
+
+    public double getPrice() {
         return price;
     }
-    public void setPrice(double p){
+
+    public void setPrice(double p) {
         price = p;
-    } 
-    public void addToFavorites(){
-     favorite = true;   
+    }
+
+    public void addToFavorites() {
+        favorite = true;
+    }
+    
+    public int getNumSongsInLibrary() {
+        return numSongsInLibrary;
     }
 }
